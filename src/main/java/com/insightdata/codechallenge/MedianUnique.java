c8b7bdd5cbd6af4f14bb9d8f026f5e1e82fe0122
SD <donachys@gmail.com>
2015-07-09 17:08:40 -0400

    buffer writing

--- src/main/java/com/insightdata/codechallenge/MedianUnique.java
+++ src/main/java/com/insightdata/codechallenge/MedianUnique.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -13,71 +15,75 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class MedianUnique{
-    private static final Pattern WHITESPACE = Pattern.compile("\\s");
-    private static final DecimalFormat fmt = new DecimalFormat("0.##");
-    public static void main(String... args) {
-        try {
-            long[] unique_word_occurences = new long[70];//buckets for storing frequency of occurences
-            BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ));
-            long num_lines = 0;
+    private static final Pattern WHITESPACE = Pattern.compile( "\\s" );
+    private static final DecimalFormat fmt = new DecimalFormat( "0.##" );
+    public static void main( String... args ) {
+        try(BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ));
+            BufferedWriter writer = new BufferedWriter( new OutputStreamWriter (System.out ));){
+            long[] uniqueWordOccurrences = new long[70];//buckets for storing frequency of occurences
+            long numLines = 0;
             String line = reader.readLine();
             while( line != null ){//read all lines
-                num_lines++;//counting number of lines begins at 1
-                String[] words = WHITESPACE.split(line);
-                int num_unique = new HashSet<String>(Arrays.asList(words)).size();
-                unique_word_occurences[num_unique]++;
-                System.out.println(fmt.format(computeMedian(unique_word_occurences, num_lines)));
+                numLines++;//counting number of lines begins at 1
+                String[] words = WHITESPACE.split( line );
+                int numUnique = new HashSet<String>(Arrays.asList( words )).size();
+                uniqueWordOccurrences[numUnique]++;
+                writer.write(fmt.format(computeMedian( uniqueWordOccurrences, numLines )));
+                writer.newLine();
+                writer.flush();
                 line = reader.readLine();
             }
-        }catch(IOException e) {
-            System.err.println("ERROR: Could not read from stdin. Usage: MedianUnique < <input_file> > <output_file>");
+        }catch( IOException e ) {
+            System.err.println( "ERROR: Could not read from stdin. "
+                                + "Usage: MedianUnique < <input_file> > <output_file>" );
         }
     }
     /**
     * The computeMedian method represents an algorithm specific to the problem of
-    * computing a median every time a new value is added. Constraints are that values
-    * are in a finite set of natural numbers. How often a value is seen is tracked in 
-    * uwo.
-    * @param uwo the unique wordcount occurrences
-    * @param total_seen the total occurrences seen (this will be the sum of all uwo elements)
+    * computing a median every time a new value is added to an array of occurences. 
+    * Constraints are that values are in a finite set of natural numbers. How often
+    * a value is seen is tracked in uwo.
+    * @param  uwo        the unique wordcount occurrences
+    * @param  totalSeen the total occurrences seen (this will be the sum of all uwo elements)
+    * @return            the median
     **/
-    public static float computeMedian(long[] uwo, long total_seen) {
-        long half = total_seen/2;
+    public static float computeMedian( long[] uwo, long totalSeen ) {
+        long half = totalSeen/2;
         int cursor = 0;
         long sum = 0;
-        if(total_seen == 1) {//special case, first entry
-            while(uwo[cursor] <= 0){
+        if( totalSeen == 1 ){//special case, first entry
+            while( uwo[cursor] <= 0 ){
                 cursor++;
             }
             return cursor;
         }
-        while( sum < half ) {//sum the elements until we reach the middle value
+        while( sum < half ){//sum the elements until we reach the middle value
             sum += uwo[cursor];
-            if( sum < half ) {//only move our cursor if we aren't done
+            if( sum < half ){//only move our cursor if we aren't done
                 cursor++;
             }
         }
-        if(sum == half && total_seen%2 == 0) {//we landed on a boundary and have an even number of values
+        if( sum == half && totalSeen%2 == 0 ){//we landed on a boundary and have an even number of values
             int next = cursor+1;
-            while(uwo[next] <= 0) 
+            while( uwo[next] <= 0 ) {
                 next++;
             }
             return computeMean(cursor, next);
-        }else if(sum == half) {//we landed on a boundary but have an odd number of values, 
+        }else if( sum == half ){//we landed on a boundary but have an odd number of values, 
                                //the next bucket with an element is the median
             int next = cursor+1;
-            while(uwo[next] <= 0) {
+            while( uwo[next] <= 0 ){
                 next++;
             }
             return next;
-        }else {
+        }else{
             return cursor;
         }
     }
     /**
     * computeMean is a helper method for the computeMedian method
-    * @param a first number to be averaged
-    * @param b second number to be averaged 
+    * @param     a first number to be averaged
+    * @param     b second number to be averaged 
     **/
     private static float computeMean(int a, int b) {
         return (a+b) / 2.0f;
--- src/main/java/com/insightdata/codechallenge/WordsTweeted.java
+++ src/main/java/com/insightdata/codechallenge/WordsTweeted.java
@@ -7,35 +7,44 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import scala.Tuple2;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class WordsTweeted {
-    private static final Pattern WHITESPACE = Pattern.compile("\\s");
+    private static final Pattern WHITESPACE = Pattern.compile( "\\s" );
 
     public static void main( String... args ) {
 
-        if (args.length < 1) {
-            System.err.println("Usage: WordsTweeted <input_file> > <output_file>");
+        if (args.length < 1){
+            System.err.println( "Usage: WordsTweeted <input_file> > <output_file>" );
             System.exit(1);
         }
+        try(BufferedWriter writer = new BufferedWriter( new OutputStreamWriter (System.out ));){
+            SparkConf conf = new SparkConf().setAppName( "WordsTweeted" );
+            JavaSparkContext spark = new JavaSparkContext( conf );
 
-        SparkConf conf = new SparkConf().setAppName("WordCount");
-        JavaSparkContext spark = new JavaSparkContext(conf);
-
-        JavaRDD<String> text_file = spark.textFile(args[0], 1);
-        JavaRDD<String> words = text_file.flatMap(line -> Arrays.asList(
-                        WHITESPACE.split(line)));
-        JavaPairRDD<String, Integer> counts = words.mapToPair(
-                        w -> new Tuple2<String, Integer>(w, 1))
-                        .reduceByKey((x, y) -> x + y);
-
-
-        List<Tuple2<String, Integer>> output = counts.sortByKey().collect();
-        for (Tuple2<String,Integer> tuple : output) {
-            System.out.println(tuple._1() + " " + tuple._2());
+            JavaRDD<String> text_file = spark.textFile( args[0], 1 );//turn our input into RDD
+            JavaRDD<String> words = text_file.flatMap( line -> Arrays.asList(
+                            WHITESPACE.split( line )));//separate into words
+            JavaPairRDD<String, Integer> counts = words.mapToPair(
+                            w -> new Tuple2<String, Integer>( w, 1 ))
+                            .reduceByKey(( x, y ) -> x + y );//combine words and count
+            //sort by the key(unique word) and collect the results to print to a file
+            //note that for large amounts of tweets, collecting could be problematic
+            List<Tuple2<String, Integer>> output = counts.sortByKey().collect();
+            for ( Tuple2<String,Integer> tuple : output ){
+                writer.write( tuple._1() + " " + tuple._2() );//print results
+                writer.newLine();
+                writer.flush();
+            }
+            spark.stop();
+        }catch(IOException e){
+            System.err.println( "ERROR: Could not write to stdout" );
         }
-        spark.stop();
     }
 }
