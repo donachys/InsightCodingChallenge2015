1c7ae21dc54051bc0772db86cdaee356c30994a7
SD <donachys@gmail.com>
2015-07-09 10:47:26 -0400

    additional comments

--- src/main/java/com/insightdata/codechallenge/MedianUnique.java
+++ src/main/java/com/insightdata/codechallenge/MedianUnique.java
@@ -16,60 +16,70 @@ public class MedianUnique{
     private static final Pattern WHITESPACE = Pattern.compile("\\s");
     private static final DecimalFormat fmt = new DecimalFormat("0.##");
     public static void main(String... args) {
-        try{
+        try {
             long[] unique_word_occurences = new long[70];//buckets for storing frequency of occurences
             BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ));
-            String line = reader.readLine();
             long num_lines = 0;
+            String line = reader.readLine();
             while( line != null ){//read all lines
-                num_lines++;
+                num_lines++;//counting number of lines begins at 1
                 String[] words = WHITESPACE.split(line);
                 int num_unique = new HashSet<String>(Arrays.asList(words)).size();
                 unique_word_occurences[num_unique]++;
                 System.out.println(fmt.format(computeMedian(unique_word_occurences, num_lines)));
                 line = reader.readLine();
             }
-        }catch(IOException e){
+        }catch(IOException e) {
             System.err.println("ERROR: Could not read from stdin. Usage: MedianUnique < <input_file> > <output_file>");
         }
     }
     /**
+    * The computeMedian method represents an algorithm specific to the problem of
+    * computing a median every time a new value is added. Constraints are that values
+    * are in a finite set of natural numbers. How often a value is seen is tracked in 
+    * uwo.
     * @param uwo the unique wordcount occurrences
     * @param total_seen the total occurrences seen (this will be the sum of all uwo elements)
     **/
-    public static float computeMedian(long[] uwo, long total_seen){
+    public static float computeMedian(long[] uwo, long total_seen) {
         long half = total_seen/2;
         int cursor = 0;
         long sum = 0;
-        if(total_seen == 1){//special case, first entry
+        if(total_seen == 1) {//special case, first entry
             while(uwo[cursor] <= 0){
                 cursor++;
             }
             return cursor;
         }
-        while( sum < half ){//sum the elements until we reach the middle
+        while( sum < half ) {//sum the elements until we reach the middle value
             sum += uwo[cursor];
-            if( sum < half ){//only move our cursor if we aren't done
+            if( sum < half ) {//only move our cursor if we aren't done
                 cursor++;
             }
         }
-        if(sum == half && total_seen%2 == 0){//we landed on a boundary and have an even number of values
+        if(sum == half && total_seen%2 == 0) {//we landed on a boundary and have an even number of values
             int next = cursor+1;
-            while(uwo[next] <= 0){
+            while(uwo[next] <= 0) 
                 next++;
             }
             return computeMean(cursor, next);
-        }else if(sum == half){//we landed on a boundary but have an odd number of values, the next bucket with an element is the median
+        }else if(sum == half) {//we landed on a boundary but have an odd number of values, 
+                               //the next bucket with an element is the median
             int next = cursor+1;
-            while(uwo[next] <= 0){
+            while(uwo[next] <= 0) {
                 next++;
             }
             return next;
-        }else{
+        }else {
             return cursor;
         }
     }
-    private static float computeMean(int a, int b){
+    /**
+    * computeMean is a helper method for the computeMedian method
+    * @param a first number to be averaged
+    * @param b second number to be averaged 
+    **/
+    private static float computeMean(int a, int b) {
         return (a+b) / 2.0f;
     }
 }
