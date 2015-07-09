53ae3b94b0a369b36f8ede86959288a900fd945d
SD <donachys@gmail.com>
2015-07-09 17:08:01 -0400

    new tests for code coverage

--- src/test/java/com/insightdata/codechallenge/MedianUniqueTest.java
+++ src/test/java/com/insightdata/codechallenge/MedianUniqueTest.java
@@ -8,34 +8,60 @@ import junit.framework.TestSuite;
  * Unit test for MedianUnique.
  */
 public class MedianUniqueTest 
-    extends TestCase
-{
+    extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public MedianUniqueTest( String testName )
-    {
+    public MedianUniqueTest( String testName ) {
         super( testName );
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite( MedianUniqueTest.class );
     }
 
-    /**
-     * 
-     */
-    public void testComputeMedian()
-    {
-        long[] values = {10l, 10l, 11l};
+    public void testNormalComputeMedian() {
+        long[] values = { 10l, 10l, 11l };
         long element_sum = 31;
-        System.err.println(MedianUnique.computeMedian(values, element_sum));
-        assertTrue( MedianUnique.computeMedian(values, element_sum) == 1.0f );
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 1.0f );
+    }
+    public void testOneElementComputeMedian() {
+        long[] values = { 0l, 1l, 0l };
+        long element_sum = 1;
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 1.0f );
+    }
+    public void testEmptyBucketEvenComputeMedian() {
+        long[] values = { 12l, 3l, 0l, 10l };
+        long element_sum = 30;
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 2.0f );
+    }
+    public void testEmptyBucketOddComputeMedian() {
+        long[] values = { 12l, 8l, 0l, 1l, 20l };
+        long element_sum = 41;
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 3.0f );
+    }
+    public void testLastBucketEvenComputeMedian() {
+        long[] values = { 12l, 0l, 0l, 18l };
+        long element_sum = 30;
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 3.0f );
+    }
+    public void testLastBucketOddComputeMedian() {
+        long[] values = { 12l, 0l, 0l, 17l };
+        long element_sum = 29;
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 3.0f );
+    }public void testFirstBucketEvenComputeMedian() {
+        long[] values = { 12l, 0l, 0l, 2l };
+        long element_sum = 14;
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 0.0f );
+    }
+    public void testFirstBucketOddComputeMedian() {
+        long[] values = { 2l, 1l, 0l, 0l };
+        long element_sum = 3;
+        assertEquals( MedianUnique.computeMedian(values, element_sum), 0.0f );
     }
 }
